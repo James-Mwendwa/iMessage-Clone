de492ce3af68abb096531de1b595d2ab011e4555
James Mwendwa <mwendwajames32@gmail.com>
2022-06-22 22:44:57 +0300

    Firebase

---
+++ src/Firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB84CK0UQ2UWndXo2PciSiplu5gFY_9dng",
+  authDomain: "imessage-clone-f3e19.firebaseapp.com",
+  projectId: "imessage-clone-f3e19",
+  storageBucket: "imessage-clone-f3e19.appspot.com",
+  messagingSenderId: "374109331111",
+  appId: "1:374109331111:web:b3ebbcf80228dafa8d4582",
+  measurementId: "G-8GNBTWFVZK",
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
